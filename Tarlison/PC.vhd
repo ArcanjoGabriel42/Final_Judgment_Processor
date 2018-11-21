9fd79d706252a4d5947b459741cbca8b4cb83e0b
Miller <millerraycell@gmail.com>
2018-11-21 19:21:40 -0400

    Certo
    PC

--- Tarlison/PC.vhd
+++ Tarlison/PC.vhd
@@ -4,7 +4,7 @@ USE IEEE.STD_LOGIC_1164.ALL;
 ENTITY PC IS
 PORT
 (
-	clk : IN STD_LOGIC;
+	clk,ativo : IN STD_LOGIC;
 	pin : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
 	pout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
 END PC;
@@ -12,4 +12,12 @@ END PC;
 ARCHITECTURE behavior OF PC IS
 BEGIN
 	pout <= pin WHEN clk = '1';
+	PROCESS(clk)
+		BEGIN
+			IF (clk = '1' AND clk'EVENT) THEN
+				IF (ativo = '1') THEN
+					pout <= pin;
+				END IF;
+			END IF;
+	END PROCESS;
 END behavior;
