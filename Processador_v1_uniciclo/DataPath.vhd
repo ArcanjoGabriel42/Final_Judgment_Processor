ef32e97059c8aafd7dfbd9156d5bb5d9e902c09e
Gabriel Arcanjo <gabriel_arcanjo_campelo@hotmail.com>
2018-11-27 09:18:25 -0400

    Possíveis Alterações Futuras
    Inout para Signal

--- Processador_v1_uniciclo/DataPath.vhd
+++ Processador_v1_uniciclo/DataPath.vhd
@@ -6,9 +6,18 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 ENTITY DataPath IS
 	PORT
 	(
-				--Clock Geral do Sistema--
+		--------Clock Geral do Sistema-------------------------------
 		Clock_Sistema:               in  std_logic;
 		-------------------------------------------------------------
+		
+		
+		--------Entradas e Saídas do PC------------------------------
+		SomadorToPc: 			        	 inout std_logic_vector(15 downto 0);
+		SaidaPc: 			  			  	 inout std_logic_vector(15 downto 0);
+		-------------------------------------------------------------
+		-------Entradas do Banco de Registradores--------------------
+		SaidaRegA:  			  		  	 inout std_logic_vector(15 downto 0);
+		SaidaRegB:  			  		  	 inout std_logic_vector(15 downto 0);
 		
 		--------Saidas da Memoria de instrução-----------------------
 		Instruction_to_multiplexador:	 inout  std_logic_vector(2  downto 0);
@@ -109,22 +118,17 @@ END COMPONENT;
 
 	COMPONENT BancoRegistradores IS
 		PORT(
-			Clock:  in std_logic;
-			EscReg: in std_logic; 							 -- Sinal da unidade de controle
+			Clock:  in  std_logic;
+			EscReg: in  std_logic; 							 -- Sinal da unidade de controle
 			RegA:   out std_logic_vector (15 downto 0);
 		   RegB:   out std_logic_vector (15 downto 0); 
-		   Data:   in std_logic_vector (15 downto 0); -- Dado a ser escrito
-			RegDst: in std_logic_vector (2 downto 0);  -- Registrador de destino
-			LeReg1: in std_logic_vector (2 downto 0);  -- Endereço do resgistrador 1
-			LeReg2: in std_logic_vector (2 downto 0)   -- Endereço do resgistrador 2
+		   Data:   in  std_logic_vector (15 downto 0); -- Dado a ser escrito
+			RegDst: in  std_logic_vector (2  downto 0);  -- Registrador de destino
+			LeReg1: in  std_logic_vector (2  downto 0);  -- Endereço do resgistrador 1
+			LeReg2: in  std_logic_vector (2  downto 0)   -- Endereço do resgistrador 2
 		);
 END COMPONENT;
 		
-	
-	SIGNAL SomadorToPc      :  std_logic_vector(15 downto 0);
-	SIGNAL SaidaPc          :  std_logic_vector(15 downto 0);
-	SIGNAL SaidaRegA        :  std_logic_vector(15 downto 0);
-	SIGNAL SaidaRegB        :  std_logic_vector(15 downto 0);
 
 
 BEGIN
@@ -135,8 +139,10 @@ G1: PC           		  port map (Clock_Sistema, SomadorToPc, SaidaPc);
 G2: SomadorPC    		  port map (SaidaPc, SomadorToPc);
 G3: memoria_ROM2 		  port map (Clock_Sistema, SaidaPc, Instruct_out, Instruction_to_Control, Instruction_to_register1, Instruction_to_register2, 
 										   Instruction_to_multiplexador,Instruction_to_controlULA, Instruction_to_is_BEQ, Instruction_to_Jump);
+											
 G4: UnidadedeControle  port map (Instruction_to_Control, Flag_regdest, Flag_origialu, Flag_memparareg, Flag_escrevereg, Flag_lemem, Flag_escrevemem,
 										   Flag_branch, Flag_aluop1, Flag_aluop0);								 
+											
 G5: Multiplexador2x1   port map (Instruction_to_register2,Instruction_to_multiplexador,Flag_regdest,multiplexador_to_writeRegister);
 G6: BancoRegistradores port map (Clock_Sistema, Flag_escrevereg, SaidaRegA,SaidaRegB,Data_to_writeRegister, multiplexador_to_writeRegister,
 											Instruction_to_register1, Instruction_to_register2);
